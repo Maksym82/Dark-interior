a783c8f82329c081c069c720472305f5c54b0903
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-10-17 13:44:37 +0300

    Update styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,25 @@
-import { c } from '../vendor/swiper/shared/utils.min.mjs';
-import Swiper from '../vendor/swiper/swiper-bundle.min.mjs';
-import { themeSwitcher } from '../vendor/theme-switcher/theme-switcher.js';
-new themeSwitcher()
+import { c } from "../vendor/swiper/shared/utils.min.mjs";
+import Swiper from "../vendor/swiper/swiper-bundle.min.mjs";
+import { themeSwitcher } from "../vendor/theme-switcher/theme-switcher.js";
+new themeSwitcher();
 
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.main-menu ul');
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".main-menu ul");
 
-menuBtn.addEventListener('click', function() {
-    this.classList.toggle('active')
-    menu.classList.toggle('active')
-})
+menuBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  menu.classList.toggle("active");
+});
 
-const swiper = new Swiper('.head-slider', {
-    effect: 'slide',
-    speed: 1000,
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 30,
-})
+const swiper = new Swiper(".head-slider", {
+  effect: "slide",
+  speed: 1000,
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
